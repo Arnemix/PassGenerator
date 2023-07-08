4b6a0abf213116228b535b3759cb63dd94274822
Arnemix <amindrayef@gmail.com>
2023-07-08 23:55:17 +0200

    🛠️ Testing & window design

--- src/main/java/be/mohamed/passgenerator/AppWindow.java
+++ src/main/java/be/mohamed/passgenerator/AppWindow.java
@@ -3,6 +3,7 @@ package be.mohamed.passgenerator;
 import javax.swing.*;
 import java.awt.*;
 import java.net.PasswordAuthentication;
+import java.util.UUID;
 
 public class AppWindow extends JFrame {
 
@@ -16,41 +17,45 @@ public class AppWindow extends JFrame {
 
 
 
-
-    public AppWindow() throws HeadlessException {
-
+    public AppWindow() {
         setTitle("Générateur de mots de passe");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setSize(800, 600); // Agrandit la fenêtre (largeur x hauteur)
+        setSize(650, 190); // Agrandit la fenêtre (largeur x hauteur)
 
-        BoxLayout layout = new BoxLayout(getContentPane(), BoxLayout.Y_AXIS);
-        getContentPane().setLayout(layout);
+        JPanel contentPane = new JPanel();
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 
-        includeNumbers = new JCheckBox("Inclure des nombres ");
-        includeSymbols = new JCheckBox("Inclure des symboles ");
-        includeLowAndUpperCase = new JCheckBox("Inclure des majuscules/miniscules ");
+        includeNumbers = new JCheckBox("Inclure des nombres");
+        includeSymbols = new JCheckBox("Inclure des symboles");
+        includeLowAndUpperCase = new JCheckBox("Inclure des majuscules/minuscules");
 
-        password = new JPasswordField(20);
+        password = new JTextField(20);
         password.setEditable(false);
 
-
         generatePassword = new JButton("Générer le mot de passe");
+
         generatePassword.addActionListener(e -> {
-            System.out.println("Génération ...");
+            password.setText(UUID.randomUUID().toString());
         });
 
-        add(includeNumbers);
-        add(includeSymbols);
-        add(includeLowAndUpperCase);
-        add(generatePassword);
-        add(password);
 
-        pack();
 
+        includeNumbers.setAlignmentX(Component.CENTER_ALIGNMENT);
+        includeSymbols.setAlignmentX(Component.CENTER_ALIGNMENT);
+        includeLowAndUpperCase.setAlignmentX(Component.CENTER_ALIGNMENT);
+        generatePassword.setAlignmentX(Component.CENTER_ALIGNMENT);
+        password.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        contentPane.add(includeNumbers);
+        contentPane.add(includeSymbols);
+        contentPane.add(includeLowAndUpperCase);
+        contentPane.add(generatePassword);
+        contentPane.add(password);
+
+        setContentPane(contentPane);
+        setLocationRelativeTo(null);
         setVisible(true);
-
-
 
 
     }
